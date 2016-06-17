2e89da8b4b7fddc83729f5326e8c5b99f2f28159
wiaofeng wu <qiaofeng@qiaofengdemacbook-pro.local>
2016-06-17 16:21:55 +0800

    Initial Commit

---
+++ ailibuli/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ailibuli
+//
+//  Created by qiaofeng wu on 16/6/17.
+//  Copyright © 2016年 Qiaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
